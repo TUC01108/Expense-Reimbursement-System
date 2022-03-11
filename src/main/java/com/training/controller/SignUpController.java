ef7a9761ed2c53132b4f5512d73e59c61fbfe5a9
TUC01108 <edwinthomaswinter@gmail.com>
2022-03-11 11:11:40 -0800

    updated controllers

--- src/main/java/com/training/controller/SignUpController.java
+++ src/main/java/com/training/controller/SignUpController.java
@@ -3,10 +3,12 @@ package com.training.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.training.ers.dao.LoginDAO;
 import com.training.ers.dao.LoginDAOImpl;
@@ -31,19 +33,24 @@ public class SignUpController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
 		
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		String gender = request.getParameter("gender");
-		String qualification = request.getParameter("qualification");
-		String notification[] = request.getParameterValues("notification");
+		String firstname = request.getParameter("first_name");
+		String lastname = request.getParameter("last_name");
+		String email = request.getParameter("email");
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("username", username);
+		/*String notification[] = request.getParameterValues("notification");
 		String finalNotification = "";
 		for(String temp:notification) {
 			finalNotification += temp +":";
-		}
+		}*/
 		
-		User user = new User(-1, username, password, gender, finalNotification, qualification);
-		
+		//User user = new User(-1, username, password, gender, finalNotification, qualification);
+		User user = new User(-1, username, password, firstname, lastname, email);		
 		LoginDAO loginDAO = new LoginDAOImpl();
 		loginDAO.register(user);
 		
@@ -55,11 +62,13 @@ public class SignUpController extends HttpServlet {
 		out.println("Welcome, "+username);
 		out.println("You are registered successfully!");
 		out.println("Password : "+password);
-		out.println("Gender : "+gender);
-		out.println("Qualification : "+finalNotification);
-		out.println("Qualification : "+qualification);
+		out.println("First Name : "+firstname);
+		out.println("Last Name : "+lastname);
+		out.println("Email : "+email);
 		
 		out.println("</body></html>");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.include(request, response);
 	}
 
 	/**
--- src/main/webapp/displayUserDetails.jsp
+++ src/main/webapp/displayUserDetails.jsp
@@ -9,29 +9,37 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
-<link type="text/css" rel="stylesheet" href="styles.css">
+<title>Display Users</title>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+	crossorigin="anonymous">
 </head>
 <body>
 	<% String username = (String)session.getAttribute("username"); %>
-		<h2>You are logged in as : 
+		<h4>You are logged in as : 
 		<%= username %> 
 		and the message is : 
-		<%= session.getAttribute("message") %></h2>
+		<%= session.getAttribute("message") %></h4>
 	<%
 	LoginDAO loginDAO = new LoginDAOImpl();
 	List<User> users = loginDAO.getUsers();
 	Iterator<User> iterator = users.iterator();
 	%>
-	<h1>List of all the users</h1>
-	<table border="2" cellspacing="10" cellpadding="10">
+	<h4 align="center">List of all the users</h4>
+	<table class="table table-dark">
+	<thead>
+	<tr>
 		<th>User Id</th>
 		<th>Username</th>
 		<th>Password</th>
-		<th>Gender</th>
-		<th>Notification</th>
-		<th>Qualification</th>
-
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th>Email</th>
+		<th>Login_id</th>
+		</tr>
+	</thead>
 		<%
 		while (iterator.hasNext()) {
 			User user = iterator.next();
@@ -41,13 +49,16 @@
 			<td><%= user.getUserId() %></td>
 			<td><%= user.getUsername() %></td>
 			<td><%= user.getPassword() %></td>
-			<td><%= user.getGender() %></td>
-			<td><%= user.getNotification() %></td>
-			<td><%= user.getQualification() %></td>
+			<td><%= user.getFirstname() %></td>
+			<td><%= user.getLastname() %></td>
+			<td><%= user.getEmail() %></td>
+			<td><%= user.getLogin_id() %></td>
 			</tr>
 		<%}
 		%>
 </table>
+<a href="welcome.jsp">Back</a>
+<br/>
 <a href="index.jsp">Logout</a>
 </body>
 </html>
